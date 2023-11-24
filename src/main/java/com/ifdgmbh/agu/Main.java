78f1b0c1d1c28dc3dae1f67631410d77107d549e
Artem Gubin <artem.gubin@ifd-gmbh.com>
2023-11-24 09:50:31 +0100

    Initial commit

---
+++ src/main/java/com/ifdgmbh/agu/Main.java
@@ -0,0 +1,10 @@
+package com.ifdgmbh.agu;
+
+public class ASCIIFontGenerator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
---
+++ src/main/java/com/ifdgmbh/agu/Solution.java
@@ -0,0 +1,2 @@
+package com.ifdgmbh.agu;public class Solution {
+}
